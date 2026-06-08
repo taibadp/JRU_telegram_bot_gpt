3fab69bcb0fc38c8f8fa1a480c693c9742a10c21
Matveev Maksim <thematveev21@gmail.com>
2026-06-08 19:47:46 +0200

    feat: task 1 and dotenv

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
+from http.client import responses
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
 
 from gpt import ChatGptService
 from util import (load_message, send_text, send_image, show_main_menu,
-                  default_callback_handler)
+                  default_callback_handler, load_prompt, send_text_buttons)
 
 import credentials
 
@@ -35,13 +37,37 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     })
 
 
+async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_image(update, context, 'random')
+    prompt = load_prompt('random')
+    # response = await chat_gpt.send_question(prompt, 'Давай рандомний факт')
+    response = 'Interesting fact'
+    # await send_text(update, context, response)
+    await send_text_buttons(
+        update, context,
+        response,
+        {
+            'random_finish' : 'Закінчити',
+            'random_one_more' :'Хочу ще факт',
+        }
+    )
+
+async def random_buttons_handler(update: Update, context):
+    query = update.callback_query.data
+    if query == 'random_finish':
+        await start(update, context)
+    elif query == 'random_one_more':
+        await random(update, context)
+    await update.callback_query.answer()
+
 chat_gpt = ChatGptService(credentials.ChatGPT_TOKEN)
 app = ApplicationBuilder().token(credentials.BOT_TOKEN).build()
 
 # Зареєструвати обробник команди можна так:
 app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('random', random))
 
 # Зареєструвати обробник колбеку можна так:
-# app.add_handler(CallbackQueryHandler(app_button, pattern='^app_.*'))
-app.add_handler(CallbackQueryHandler(default_callback_handler))
+app.add_handler(CallbackQueryHandler(random_buttons_handler, pattern='^random_.*'))
+# app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
--- credentials.py
+++ credentials.py
@@ -1,2 +1,7 @@
-ChatGPT_TOKEN = ""
-BOT_TOKEN = ""
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+ChatGPT_TOKEN = os.environ['CHATGPT_TOKEN']
+BOT_TOKEN = os.environ['BOT_TOKEN']
